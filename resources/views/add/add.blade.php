d5fd78606759c85f337402cfbce6ca19bb40554d
xingxuan <1171778420@qq.com>
2020-02-29 16:17:11 +0800

    laravel框架

---
+++ resources/views/add/add.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="get" accept-charset="utf-8">
+
+        <input type="text" name="name">
+        <input type="text" name="pwd">
+        <input type="submit" value="添加"/>
+
+    </form>
+</body>
+</html>
